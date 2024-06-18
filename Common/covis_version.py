5f37c29e8963b2cb1125dfb7da91c867532e2059
lcm <casimirliu1999@gmail.com>
2024-06-18 10:10:54 +0800

    first add

---
+++ Common/covis_version.py
@@ -0,0 +1,7 @@
+def covis_version():
+    version_number = '2.1.2'
+    version_data = {
+        'version_number' : version_number,
+        'version_string' : 'COVIS Post-processing in Python v' + version_number
+    }
+    return version_data
